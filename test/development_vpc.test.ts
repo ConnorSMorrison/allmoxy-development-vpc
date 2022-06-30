c24baf18b17cabf4dcfd3ce2efb20bab14ffbb93
Connor Morrison <csmorrison06@gmail.com>
2022-06-29 21:57:44 -0600

    Initial commit

---
+++ test/development_vpc.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DevelopmentVpc from '../lib/development_vpc-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/development_vpc-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DevelopmentVpc.DevelopmentVpcStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
