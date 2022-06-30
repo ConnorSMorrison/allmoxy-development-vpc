ac054670eee3a12665fb06cfef9be313e1a67446
Connor Morrison <csmorrison06@gmail.com>
2022-06-30 12:09:30 -0600

    initial commit

--- lib/development_vpc-stack.ts
+++ lib/development_vpc-stack.ts
@@ -1,16 +1,34 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
 export class DevelopmentVpcStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'DevelopmentVpcQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+		const vpc = new ec2.Vpc(this, "Vpc", {
+			cidr: "10.16.0.0/16",
+			natGateways: 0,
+			maxAzs: 3,
+			subnetConfiguration: [
+				{
+					name: "IT",
+					subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+				},
+				{
+					name: "DB",
+					subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+				}, 
+				{
+					name: "APP",
+					subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+				}, 
+				{
+					name: "WEB",
+					subnetType: ec2.SubnetType.PUBLIC,
+				}
+			]
+		});
   }
 }
