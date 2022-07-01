4631e5930838cc5d08283e2f733da8fea7cd8e4f
Connor Morrison <csmorrison06@gmail.com>
2022-07-01 16:10:10 -0600

    made it work now

--- lib/development_vpc-stack.ts
+++ lib/development_vpc-stack.ts
@@ -1,34 +1,29 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
 export class DevelopmentVpcStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
-		const vpc = new ec2.Vpc(this, "Vpc", {
-			cidr: "10.16.0.0/16",
-			natGateways: 0,
-			maxAzs: 3,
-			subnetConfiguration: [
-				{
-					name: "IT",
-					subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
-				},
-				{
-					name: "DB",
-					subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
-				}, 
-				{
-					name: "APP",
-					subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
-				}, 
-				{
-					name: "WEB",
-					subnetType: ec2.SubnetType.PUBLIC,
-				}
-			]
-		});
+	
+   	let subnetConfigValues = {
+   		 "IT": ec2.SubnetType.PRIVATE_ISOLATED, "DB": ec2.SubnetType.PRIVATE_ISOLATED,
+   		 "APP": ec2.SubnetType.PRIVATE_ISOLATED, "WEB": ec2.SubnetType.PUBLIC};
+	let cidrMask = 20;
+	let newSubnetConfiguration = [];
+		
+	for (let i = 0; i < Object.keys(subnetConfigValues).length; i++) {
+		let subnetConfigValuesNames = Object.keys(subnetConfigValues);
+		let subnetConfigValuesTypes = Object.values(subnetConfigValues);
+		newSubnetConfiguration.push({name: subnetConfigValuesNames[i], 
+			subnetType: subnetConfigValuesTypes[i],
+			cidrMask: cidrMask});
+	}
+	
+	const vpc = new ec2.Vpc(this, "Vpc", {
+		cidr: "10.16.0.0/16",
+		natGateways: 0,
+		subnetConfiguration: newSubnetConfiguration,
+	});
   }
 }
